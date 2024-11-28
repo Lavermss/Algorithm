7ca51fac9e17b684cf78e483cc79ebc13b43740a
Lavermss <thxsoo123@gmail.com>
2024-11-28 23:18:57 +0900

    [Bronze V] Title: A/B, Time: 184 ms, Memory: 17956 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
 
 public class Main{
-
-public static void main(String[]args){
-    
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
         
-        double a = scanner.nextDouble();
-        double b = scanner.nextDouble();
-    
-        scanner.close();
-        System.out.print(a/b);
-}
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        
+        sc.close();
+        System.out.println(a/b);
+    }
 }
